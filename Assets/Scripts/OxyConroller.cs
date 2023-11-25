4984eb3659d48f97d90ebca8be085a71610d8b0c
Monika Bošániová <monika.bosaniova@gmail.com>
2023-11-25 21:28:24 +0100

    random added values to temp and oxygen

--- Assets/Scripts/OxyConroller.cs
+++ Assets/Scripts/OxyConroller.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine.UI;
 
 public class OxyConroller : MonoBehaviour
 {
-    public Slider slider;
+    public Transform Rotator;
     public StatsManager statsManager;
 
     [SerializeField]
@@ -16,10 +17,12 @@ public class OxyConroller : MonoBehaviour
     private float minimumAngle = 120;
     private float maximumAngle = -30;
 
+    private float probability = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        slider.value = statsManager.Oxygen;
+        SetValueOnSlider(statsManager.Oxygen);
         MinAcceptedValue = statsManager.MinOxygen;
         MaxAcceptedValue = statsManager.MaxOxygen;
     }
@@ -27,7 +30,10 @@ public class OxyConroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Random.value < probability)
+        {
+            statsManager.Oxygen = Mathf.Clamp01(statsManager.Oxygen + 0.01f);
+        }
     }
 
     public void AddToOxygen(float value)
@@ -38,6 +44,8 @@ public class OxyConroller : MonoBehaviour
 
     public void SetValueOnSlider(float value)
     {
-        slider.value = value;
+        float z = Mathf.LerpAngle(minimumAngle, maximumAngle, value);
+        Rotator.DORotate(new Vector3(0, 0, z), 0.5f);
+        //slider.value = value;
     }
 }
