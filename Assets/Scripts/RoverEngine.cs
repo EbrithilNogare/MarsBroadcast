b68ed402e59982b29902223295f7d0e2658b599c
Monika Bošániová <monika.bosaniova@gmail.com>
2023-11-25 15:33:42 +0100

    raycasting

--- Assets/Scripts/RoverEngine.cs
+++ Assets/Scripts/RoverEngine.cs
@@ -10,6 +10,8 @@ public class RoverEngine : MonoBehaviour
 
     public bool moving;
 
+    public StatsManager stats;
+
     void Start()
     {
         Move();
@@ -17,7 +19,40 @@ public class RoverEngine : MonoBehaviour
 
     void Update()
     {
+        // Perform the 2D raycast
 
+
+        // Draw a debug line in the editor
+        //Debug.DrawLine(transform.position, transform.position + new Vector3(0, 0, 1) * 10f, Color.green);
+
+
+
+        float angleInRadians = Mathf.Deg2Rad * (transform.eulerAngles.z + 5);
+        float y = Mathf.Cos(angleInRadians);
+        float x = -Mathf.Sin(angleInRadians);
+
+        RaycastHit2D hit1 = Physics2D.Raycast(transform.position, new Vector3(x, y, 0), 20f);
+        Debug.DrawLine(transform.position, new Vector3(hit1.point.x, hit1.point.y, 0), Color.green);
+
+        angleInRadians = Mathf.Deg2Rad * (transform.eulerAngles.z - 5);
+        y = Mathf.Cos(angleInRadians);
+        x = -Mathf.Sin(angleInRadians);
+
+        RaycastHit2D hit2 = Physics2D.Raycast(transform.position, new Vector3(x, y, 0), 20f);
+        Debug.DrawLine(transform.position, new Vector3(hit2.point.x, hit2.point.y, 0), Color.green);
+        //Debug.DrawLine(transform.position, new Vector3(x, y, 0) * 1000f, Color.green);
+
+        // Check if the ray hits something
+        if (hit1.collider != null && hit2.collider != null &&
+            hit1.collider.transform.gameObject.tag == "Screen" &&
+            hit2.collider.transform.gameObject.tag == "Screen")
+        {
+            //Debug.Log("Ray hit: " + hit1.collider.name + hit2.collider.name);
+        }
+        else
+        {
+            Debug.Log("ROVER SAW US!!");
+        }
     }
 
     private void Move()
