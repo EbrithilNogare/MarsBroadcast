4658b3c2e1c79d322024988b3bd511c7756a1426
David Napravnik <ebrithilnogare@gmail.com>
2023-11-26 13:07:08 +0100

    balancing and new tag

--- Assets/Scripts/BatteryController.cs
+++ Assets/Scripts/BatteryController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,22 +6,14 @@ public class BatteryController : MonoBehaviour
     public Slider Slider;
     public StatsManager statsManager;
 
-    // Start is called before the first frame update
     void Start()
     {
         Slider.value = statsManager.Battery;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void AddToTemp(float value)
     {
         statsManager.Battery = Mathf.Clamp01(statsManager.Temperature + value);
-        //slider.value = statsManager.Temperature;
     }
 
     public void SetValueOnSlider(float value)
